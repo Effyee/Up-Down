02906bd7c4ccdd791dfb25c457c5f5d3a8be0ef0
gayeon7877 <gayean01@gmail.com>
2025-09-09 18:57:38 +0900

    🎉initial commit

---
+++ src/main/java/com/upanddown/upanddown/UpanddownApplication.java
@@ -0,0 +1,13 @@
+package com.upanddown.upanddown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpanddownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpanddownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upanddown/upanddown/UpanddownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upanddown.upanddown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpanddownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
